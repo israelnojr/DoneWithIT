7b5af1b99bb58467ddc4096c77f8fdd12a98f950
israelnojr <israeliloba@gmail.com>
2020-06-17 23:37:01 +0100

    clean up project dir

--- src/RootComponent.jsx
+++ src/RootComponent.jsx
@@ -2,18 +2,12 @@ import * as React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 
-import TabsController from "./components/TabsController";
-
 const Drawer = createDrawerNavigator();
 
 function RootComponent() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        {/* <Drawer.Screen name="Home" component={HomeStackScreen} options={{}} />
-        <Drawer.Screen name="Map" component={MapStackScreen} options={{}} /> */}
-        <Drawer.Screen name="Home" component={TabsController} options={{}} />
-      </Drawer.Navigator>
+      <Drawer.Navigator initialRouteName=""></Drawer.Navigator>
     </NavigationContainer>
   );
 }
